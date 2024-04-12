c9350b20851deb49becc924be5a14eefb9d1f82a
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-04-12 22:59:29 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,44 +6,38 @@ import Aos from 'aos';
 
 function Nav() {
     Aos.init();
-
-    let [navbar, setNavbar] = useState({ height: 0, padding: 0 })
-
-    const activeNav = () => {
-        if (navbar.height === 0) {
-            setNavbar({ height: "250px", padding: '20px' })
-        }
-        else {
-            setNavbar({ height: 0, padding: 0 })
-        }
-    }
     return (
         <>
-            <section>
-                <header className="header">
-                    <div className="logo" data-aos="fade" data-aos-duration="1500">
-                        <h2>S.<span>Urmila</span></h2>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                <div className="container-fluid">
+                    <a data-aos="fade" data-aos-duration="1500" className="navbar-brand" href="#">S.<span>Urmila</span></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#home" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="200">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#about" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="300">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#skills" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="400">Skills</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#services" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="500">Education</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#project" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="600">Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#contact" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="700">Contact</a>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="nav">
-                        <a href="#home" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="200">Home</a>
-                        <a href="#about" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="300">About Me</a>
-                        <a href="#skills" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="400">Skills</a>
-                        <a href="#services" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="500">Education</a>
-                        <a href="#project" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="600">Projects</a>
-                        <a href="#contact" data-aos="fade-down" data-aos-duration="1500" data-aos-delay="700">Contact</a>
-                    </div>
-                    <div className="bars" onClick={activeNav}>
-                    <input type="checkbox" id="menu-check"/>
-                    <div className="menu-btn">
-                        <label for="menu-check">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </label>
-                    </div>
-                    </div>
-                </header>
-            </section>
+                </div>
+            </nav>
         </>
     );
 };
