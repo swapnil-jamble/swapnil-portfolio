5e23051ccc3f27c257b40718a6a9f3f9f90a86d6
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-05-22 12:48:12 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
                 <div className='about'>
                     <div className='about-info'>
                         <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="400">About me</p>
-                        <h2 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="600">Shirole Urmila</h2>
+                        <h2 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="600">Takate Urmila</h2>
                         <h3 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="800">Hi, I am <span>MERN Stack<span> Developer</span></span></h3>
                         <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000">I'm passionate about creating beautiful, user-friendly web experiences. I have a strong understanding of HTML, CSS and Javascript, and I'm proficient in a variety of front-end frameworks, including React.</p><br></br>
                         <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1100">I'm also a strong believer in the importance of accessibility and usability. I always strive to create web applications that are accessible to everyone, regardless of their device or ability.</p>
