8fe0977ef91df0b1b92605b5b9bb208efccf6bed
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-05-22 12:42:00 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,7 +15,6 @@ function Project(){
                             <img src="https://bkwebdesigns.com/wp-content/uploads/2018/02/featured-8.jpg" alt="project-img" />
                             <div>
                                 <h4>Personal Portfolio</h4>
-                                <p>A good portfolio is more than a long list of good stocks and bonds.</p>
                                 <a href="#">Live Demo<i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
@@ -23,7 +22,6 @@ function Project(){
                             <img src="https://repository-images.githubusercontent.com/200318206/978d6f00-88dd-11ea-8727-1449bd91edc6" alt="project-img" />
                             <div>
                                 <h4>Weather Application</h4>
-                                <p>Weather app provide real-time weather information and forecasts.</p>
                                 <a href="https://react-weather-app-github-io.vercel.app/" target='_blank'>Live Demo <i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
@@ -31,7 +29,6 @@ function Project(){
                             <img src="https://www.builderfly.com/wp-content/uploads/2020/05/What-are-ways-to-promote-a-clothing-e-Commerce-website-min.jpg" alt="project-img" />
                             <div>
                                 <h4>E-commerce Shopping</h4>
-                                <p>E-commerce website allows people to buy and sell physical goods, services.</p>
                                 <a href="https://apparels-github-io.vercel.app/" target='_blank'>Live Demo <i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
@@ -39,7 +36,6 @@ function Project(){
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsPtL6zMy8b-Ee7tGhjCdKX7U04vdpRllESobZ3j0MYQ&s" alt="project-img" />
                             <div>
                                 <h4>Calculator Application</h4>
-                                <p>We do math operations more easily. For example- add, subtract, multiply, and divide.</p>
                                 <a href="https://react-calculator-nine-flame.vercel.app/" target='_blank'>Live Demo <i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
