4f94c4afc2bffaf5f9aed9cfb654d07efb7ba5b0
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-05-22 12:39:14 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,10 +14,10 @@ function Header(){
                         <h1 data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1000">Hi, I am <span>Shirole Urmila</span></h1>
                         <p data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1100">Do the best you can until you know better . . .</p>
                         <div className='social-icons'>
+                             <a href="https://www.linkedin.com/in/urmila-shirole-41b1b72b8/" target='_blank'><i className='fa-brands fa-linkedin' data-aos="fade" data-aos-duration="1500" data-aos-delay="1400"></i></a>
+                            <a href="https://github.com/urmilashirole" target='_blank'><i className='fa-brands fa-github' data-aos="fade" data-aos-duration="1500" data-aos-delay="1500"></i></a>
                             <a href="#"><i className='fa-brands fa-instagram' data-aos="fade" data-aos-duration="1500" data-aos-delay="1200"></i></a>
                             <a href="#"><i className='fa-brands fa-facebook' data-aos="fade" data-aos-duration="1500" data-aos-delay="1300"></i></a>
-                            <a href="https://www.linkedin.com/in/urmila-shirole-41b1b72b8/" target='_blank'><i className='fa-brands fa-linkedin' data-aos="fade" data-aos-duration="1500" data-aos-delay="1400"></i></a>
-                            <a href="https://github.com/urmilashirole" target='_blank'><i className='fa-brands fa-github' data-aos="fade" data-aos-duration="1500" data-aos-delay="1500"></i></a>
                         </div>
                         <div className='hero-btns'>
                             <a href="#skills"><button data-aos="fade-right" data-aos-duration="1500" data-aos-delay="1600">Hire me</button></a>
@@ -25,7 +25,7 @@ function Header(){
                         </div>
                     </div>
                     <div className='hero-img' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1800">
-                        <img src="https://www.wbw.org/wp-content/uploads/2016/03/Female-Avatar.png" alt="hero-img"/>
+                        <img src="./Images/profile1.png" alt="hero-img"/>
                     </div>
                 </div>
             </section>
