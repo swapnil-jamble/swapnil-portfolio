71f60d717aee26af7df48a0cd00e3770d169d928
urmilashirole <161549377+urmilashirole@users.noreply.github.com>
2024-05-22 17:13:20 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -18,21 +18,21 @@ function Project(){
                                 <a href="#">Live Demo<i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
-                        <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1000">
+                        <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="500">
                             <img src="https://repository-images.githubusercontent.com/200318206/978d6f00-88dd-11ea-8727-1449bd91edc6" alt="project-img" />
                             <div>
                                 <h4>Weather Application</h4>
                                 <a href="https://react-weather-app-github-io.vercel.app/" target='_blank'>Live Demo <i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
-                        <div className='project-card' data-aos="fade-right" data-aos-duration="1500" data-aos-delay="2000">
+                        <div className='project-card' data-aos="fade-right" data-aos-duration="1500" data-aos-delay="700">
                             <img src="https://www.builderfly.com/wp-content/uploads/2020/05/What-are-ways-to-promote-a-clothing-e-Commerce-website-min.jpg" alt="project-img" />
                             <div>
                                 <h4>E-commerce Shopping</h4>
                                 <a href="https://apparels-github-io.vercel.app/" target='_blank'>Live Demo <i className='fa-solid fa-arrow-right-long'></i></a>
                             </div>
                         </div>
-                        <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="2500">
+                        <div className='project-card' data-aos="fade-left" data-aos-duration="1500" data-aos-delay="700">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsPtL6zMy8b-Ee7tGhjCdKX7U04vdpRllESobZ3j0MYQ&s" alt="project-img" />
                             <div>
                                 <h4>Calculator Application</h4>
